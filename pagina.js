c0db8863bdfdf75789343a343a66498196dd6aac
axel <huertaaxel1003@gmail.com>
2022-08-01 23:33:07 -0300

    actualizado

---
+++ pagina.js
@@ -0,0 +1,121 @@
+let uno = document.getElementById("uno");
+let dos = document.getElementById("dos");
+let tres = document.getElementById("tres");
+let cuatro = document.getElementById("cuatro");
+let cinco = document.getElementById("cinco");
+let seis = document.getElementById("seis");
+uno.addEventListener("click", ()=>{
+    let division = document.getElementById("cont");
+    division.innerHTML = `
+    <div>
+        <section>
+            <h2>MODULO UNO</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-uno</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-uno</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-uno</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-uno</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+    </div>
+    `;
+});
+
+dos.addEventListener("click", ()=>{
+    let division = document.getElementById("cont");
+    division.innerHTML = `
+    <div>
+        <section>
+            <h2>MODULO DOS</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+    </div>
+    `;
+});
+tres.addEventListener("click", ()=>{
+    let division = document.getElementById("cont");
+    division.innerHTML = `
+    <div>
+        <section>
+            <h2>MODULO TRES</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+    </div>
+    `;
+});
+cuatro.addEventListener("click", ()=>{
+    alert("Bienvenido a la unidad: 4");
+    let division = document.getElementById("cont");
+    division.innerHTML = `
+    <div>
+        <section>
+            <h2>MODULO CUATRO</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>Pensar</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-cuatro</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-cuatro</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+        <section>
+            <h2>mundo-dos</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis reiciendis velit quisquam possimus non molestiae fuga magnam exercitationem labore qui quis nam, doloribus quos, inventore eos tempore eligendi perspiciatis. Natus!</p>
+        </section>
+    </div>
+    `;
+});
+
+
+
+
+
